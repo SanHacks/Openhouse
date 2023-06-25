dda9e0b08b0c3fa4bb5a388df8133f30e75d0f41
Gundo <sifhufhisg@gmail.com>
2023-06-25 22:36:00 +0200

    Initial commit

---
+++ Block/Image.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Gundo\Openhouse\Block;
+
+
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\View\Element\Template;
+use Magento\Catalog\Model\Category;
+
+class Image extends Template
+{
+    protected Category $category;
+
+    public function __construct(
+        Template\Context $context,
+        Category $category,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->category = $category;
+    }
+
+    /**
+     * @throws LocalizedException
+     */
+    public function getCategoryImageUrl(): bool|string
+    {
+        // Get the current category ID
+        $categoryId = $this->getRequest()->getParam('id');
+
+        // Load the category by ID
+        $category = $this->category->load($categoryId);
+
+        // Return the image URL if it exists, otherwise return a default image URL
+        if ($category->getImageUrl()) {
+            return $category->getImageUrl();
+        } else {
+            return $this->getViewFileUrl('images/default_image.jpg');
+        }
+    }
+}
+
